f910b3b2d6724d75c25a205d682b84324f13f005
Matthew Harrilal <matthewharrilal@gmail.com>
2019-02-21 16:32:13 -0800

    Added two attributes to the initalizers of the region class ... higher structure to contain all quadrantw

--- region.py
+++ region.py
@@ -1,7 +1,11 @@
 from point import Point
 
 class Region(object):
-    def __init__(self, width, height, points_collection=None): # User passes in an array of points that they want
+    def __init__(self, x_coordinate, y_coordinate, width, height, points_collection=None): # User passes in an array of points that they want
+        # Center point in which the sub quadrants are based off of
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+
         self.width = width
         self.height = height
         self.capacity = 0 # Represents counter for when to subdivide
@@ -19,5 +23,5 @@ class Region(object):
         pass
 
     def subdivide(self):
-        '''Splits the region into 4 separate quadrants'''
+        '''Splits the region into 4 separate quadrants based of center point of the region'''
         pass
